b01dbf2d7f3b5d6a1e82537f11ffcf58f99530b0
JLirik <dimdimych181007@yandex.ru>
2025-10-04 22:11:51 +0300

    web commit 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
@@ -8,10 +8,33 @@ def index():
     return render_template('news_list.html')
 
 
-@app.route('/news_page', methods=['POST'])
-def news_page():
+@app.route('/news_page/<ids>', methods=['GET'])
+def news_page(ids):
+    # вот тут передается АДИ новости - по нему надо остальную инфу
+    return render_template('news_page.html', ids=ids)
+
+
+@app.route('/get_news_id', methods=['POST'])
+def get_news_id():
     data = request.get_json()
-    return render_template('news_page.html', ids=data[0])
+    return redirect(url_for('news_page', ids=data['news_id']))
+
+
+@app.route('/set_filter', methods=['POST'])
+def get_period_data():
+    data = request.get_json()['period']
+    print(data)
+    # data 1h or 24h or 1w ВОТ ПРЯМ ТАК КАК НАПИСАНО И передавать как в формате ниже
+    return jsonify([
+        {'period': '1h'},
+        {'period': '24h'}
+    ])
+
+
+@app.route('/create_news', methods=['GET'])
+def create_news_window():
+    pass
+
 
 if __name__ == '__main__':    app.run()
 
--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,22 +1,63 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM загружен - отправляю запрос');
+    loadData();
+});
+
+
 let news_table = document.getElementById('news-container');
+let use_filter = document.getElementById('filter');
+let create_news = document.getElementById('create');
+
+use_filter.addEventListener('click', async function() {loadData();});
+
+async function loadData() {
+    let selected_element = document.getElementById('selector');
+
+    let selected_value = selected_element.value;
+    console.log(selected_value);
+    $.ajax({
+        url: '/set_filter',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            'period': selected_value
+        }),
+        success: function(data) {
+            console.log(data[0]);
+            location.reload;
+
+        },
+        error: function(error) {
+            console.log('Error!!!');
+        }
+    });
+}
+
+create_news.addEventListener('click', async function() {
+    window.location.href = '/create_news';
+})
 
 news_table.addEventListener('click', async function(event) {
     let id = event.target.id;
+    console.log(id, 1);
     if (event.target.matches('button') && id) {
-        $.ajax({
-            url: '/news_page',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({
-                'item_id': id
-            }),
-            success: function(response) {
-                console.log(response);
-                location.reload;
+        fetch('/get_news_id', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
             },
-            error: function(error) {
-                console.log('Error!!!');
+            body: JSON.stringify({
+                news_id: id
+            })
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
             }
-        });
+        })
+        .catch(error => console.error('Error:', error));
     }
 });
+
+
+
